7dbb537951b327370c45129a6d86de7bec4f2799
Altair Sossai <altairsossai@gmail.com>
2021-02-06 19:43:31 -0300

    JsonConfiguration

---
+++ Zeats.Json/JsonConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace Zeats.Json
+{
+    public static class JsonConfiguration
+    {
+        public static readonly JsonSerializerOptions Options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            IgnoreNullValues = true
+        };
+    }
+}
---
+++ Zeats.Json/JsonSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Zeats.Json
+{
+    public static class JsonSerializer
+    {
+        public static T Deserialize<T>(string json, JsonSerializerOptions options = null)
+        {
+            return System.Text.Json.JsonSerializer.Deserialize<T>(json, options ?? JsonConfiguration.Options);
+        }
+
+        public static async Task<T> DeserializeAsync<T>(Stream stream, JsonSerializerOptions options = null)
+        {
+            return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(stream, options ?? JsonConfiguration.Options);
+        }
+    }
+}
